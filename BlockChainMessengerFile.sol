65fc9dd0af73f7a28da6660f0f7efdceb09c313a
sai1505 <vsai1505.bona@gmail.com>
2026-02-15 11:09:10 +0530

    Project completed.

--- BlockChainMessengerFile.sol
+++ BlockChainMessengerFile.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-contract BloackChainMessenger {
-    string public MyString = "Hello world";
+contract BlockChainMessengerFile {
+    string public myString;
+    address owner;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function updateMyString (string memory _myString) public {
+        if (owner == msg.sender) {
+            myString = _myString;
+        }
+    }
 }
